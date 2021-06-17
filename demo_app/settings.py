8e6de27078a9c6a5a20ad75d196dac134d5fc57e
david.ahmed <davedshepherd@gmail.com>
2021-06-17 22:50:14 +0200

    Added minimal app with authentication

--- demo_app/settings.py
+++ demo_app/settings.py
@@ -10,10 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'demo_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
